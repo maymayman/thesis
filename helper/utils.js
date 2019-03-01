fb8e2be4d2c31f20ca1443c08e7668abcf3b0a07
ducnghia <nghia.hoangduc@haravan.com>
2019-03-01 14:03:16 +0700

    update

--- helper/utils.js
+++ helper/utils.js
@@ -1,8 +1,14 @@
 const validateEmail = function(email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-}
+};
+
+const validateUserName = function(username) {
+	var re = /^[a-zA-Z0-9]*$/;
+    return re.test(String(username).toLowerCase());
+};
 
 module.exports = {
-	validateEmail
+	validateEmail,
+  validateUserName
 };
