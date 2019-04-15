cc4ce4c4d186c6735d4cc957f20a65b2007f466e
maymayman <tlvduc92@gmail.com>
2019-04-15 07:55:45 +0700

    Update donates.js

--- controllers/donates.js
+++ controllers/donates.js
@@ -10,13 +10,11 @@ const { ROLE, STATUS_TYPE, STATUS} = require('../config/const').USER;
 const getByProjectId = async function (req, res) {
   try {
     const user = req.user;
+    const limit = req.query.limit ? parseInt(req.query.limit) : 20;
+    const skip = req.query.skip ? parseInt(req.query.skip) : 0;
     const projectId = req.params.projectId;
-    if (!user || !projectId){
-      
-      return ResponeSuccess(req, res, {});
-    }
     
-    const donate = await DonateService.getDonate(projectId);
+    const donate = await DonateService.getDonate({projectId} , limit, skip);
   
     return ResponeSuccess(req, res, {donate});
   
